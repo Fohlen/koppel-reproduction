4e8487640f40028b71e3190e4bc05c7aafa971e6
Fohlen <3730888+Fohlen@users.noreply.github.com>
2021-05-11 12:16:50 +0200

    Make KopplVectorizer more dynamic.

--- KopplVectorizer.py
+++ KopplVectorizer.py
@@ -1,3 +1,4 @@
+import os
 from typing import List, Tuple
 from collections import Counter
 from itertools import chain
@@ -27,51 +28,67 @@ def extract_rare_pos_tags(pos_per_text: List[Tuple[POS]]) -> List[Tuple[POS]]:
 
 
 class KopplVectorizer:
-    def __init__(self):
-        self.stopwords = load_dataset('text', data_files=['onix.txt'], split='train')
+    def __init__(self, features=None, onix_path=None):
+        if features is None:
+            features = ['stopwords', 'trigrams', 'bigrams', 'rare_pos_tags']
+        if onix_path is None:
+            onix_path = os.path.join(os.getcwd(), 'onix.txt')
+        self.stopwords = load_dataset('text', data_files=[onix_path], split='train') if 'stopwords' in features else []
         self.tokens_per_text = None
         self.pos_per_text = None
         self.trigrams_by_frequency = None
         self.bigrams_by_frequency = None
         self.rare_pos_tags_by_frequency = None
-        self.vector_length = 0
+        self.features = features
 
     def fit(self, texts):
         self.tokens_per_text = [word_tokenize(text) for text in texts]
-        self.trigrams_by_frequency = extract_trigrams(texts)
-        self.bigrams_by_frequency = extract_bigrams(texts)
-        self.pos_per_text = [pos_tag(tokens) for tokens in self.tokens_per_text]
-        self.rare_pos_tags_by_frequency = extract_rare_pos_tags(self.pos_per_text)
-        self.vector_length = len(self.stopwords) + len(self.trigrams_by_frequency) + len(self.bigrams_by_frequency) + len(self.rare_pos_tags_by_frequency)
+        self.trigrams_by_frequency = extract_trigrams(texts) if 'trigrams' in self.features else []
+        self.bigrams_by_frequency = extract_bigrams(texts) if 'bigrams' in self.features else []
+        self.pos_per_text = [pos_tag(tokens) for tokens in self.tokens_per_text] if 'rare_pos_tags' in self.features else []
+        self.rare_pos_tags_by_frequency = extract_rare_pos_tags(self.pos_per_text) if 'rare_pos_tags' in self.features else []
 
     def transform(self, texts) -> np.ndarray:
-        matrix = np.empty([len(texts), self.vector_length])
+        stopwords_enabled = 'stopwords' in self.features
+        bigrams_enabled = 'bigrams' in self.features
+        trigrams_enabled = 'trigrams' in self.features
+        rare_pos_enabled = 'rare_pos_tags' in self.features
+        vector_length = (stopwords_enabled * len(self.stopwords)) + (trigrams_enabled * len(self.trigrams_by_frequency)) + (bigrams_enabled * len(self.bigrams_by_frequency)) +  (rare_pos_enabled * len(self.rare_pos_tags_by_frequency))
+
+        matrix = np.empty([len(texts), vector_length])
         for index, text in enumerate(texts):
             tokens = word_tokenize(text)
-            vector = np.zeros(self.vector_length)
+            vector = np.zeros(vector_length)
 
-            for j, stopword in enumerate(self.stopwords['text']):
-                c = tokens.count(stopword)
-                if c:
-                    vector[j] += c
+            if stopwords_enabled:
+                for j, stopword in enumerate(self.stopwords['text']):
+                    c = tokens.count(stopword)
+                    if c:
+                        vector[j] += c
 
-            bigrams = list(ngrams(text, 2))
-            for k, bigram in enumerate(self.bigrams_by_frequency):
-                c = bigrams.count(bigram)
-                if c:
-                    vector[len(self.stopwords)+k] += c
+            if bigrams_enabled:
+                offset = (stopwords_enabled * len(self.stopwords))
+                bigrams = list(ngrams(text, 2))
+                for k, bigram in enumerate(self.bigrams_by_frequency):
+                    c = bigrams.count(bigram)
+                    if c:
+                        vector[offset+k] += c
 
-            trigrams = list(ngrams(text, 3))
-            for l, trigram in enumerate(self.trigrams_by_frequency):
-                c = trigrams.count(trigram)
-                if c:
-                    vector[len(self.stopwords)+len(self.bigrams_by_frequency)+l] += c
+            if trigrams_enabled:
+                offset = (stopwords_enabled * len(self.stopwords)) + (bigrams_enabled * len(self.bigrams_by_frequency))
+                trigrams = list(ngrams(text, 3))
+                for l, trigram in enumerate(self.trigrams_by_frequency):
+                    c = trigrams.count(trigram)
+                    if c:
+                        vector[offset+l] += c
 
-            pos_bigrams_per_text = list(ngrams(pos_tag(tokens), 2))
-            for m, pos in enumerate(self.rare_pos_tags_by_frequency):
-                c = pos_bigrams_per_text.count(pos)
-                if c:
-                    vector[len(self.stopwords)+len(self.bigrams_by_frequency)+len(self.trigrams_by_frequency)+m] += c
+            if rare_pos_enabled:
+                offset = (stopwords_enabled * len(self.stopwords)) + (bigrams_enabled * len(self.bigrams_by_frequency)) + (trigrams_enabled * len(self.trigrams_by_frequency))
+                pos_bigrams_per_text = list(ngrams(pos_tag(tokens), 2))
+                for m, pos in enumerate(self.rare_pos_tags_by_frequency):
+                    c = pos_bigrams_per_text.count(pos)
+                    if c:
+                        vector[offset+m] += c
 
             matrix[index] = vector
         return matrix
